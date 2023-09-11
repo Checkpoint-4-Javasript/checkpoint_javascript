b8f506bb48d7da589b8b244d616d98527085b7b9
Júlia Marques <128760276+juliammneves@users.noreply.github.com>
2023-09-11 16:40:41 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,7 +8,15 @@ function App() {
 
   return (
     <>
-    
+    <BrowserRouter>
+    <Nav />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Produtos' element={<Produtos/>}/>
+        <Route path='/Sobre' element={<Sobre/>}/>
+      </Routes>
+    <Footer />
+    </BrowserRouter>
     </>
   )
 }
